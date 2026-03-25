bbb00c22f23645d023a281d739106ffa5f30f858
bilas238 <rakibulislambilas@gmail.com>
2026-03-25 14:51:46 +0600

    upadate main.py

--- main.py
+++ main.py
@@ -5,29 +5,40 @@ from data_manager import DataManager
 def main():
     pygame.init()
     
-    # Screen size as a Tuple
+    # Screen size as tuple
+    
     screen_info = (800, 600)
     screen = pygame.display.set_mode(screen_info)
     pygame.display.set_caption("Space Escape")
 
+    # Defining Game Colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0)
+    
     data_handler = DataManager()
     high_score = data_handler.load_score()
 
-    # List and For Loop for showing massage 
-    start_messages = ["Loading...", "High Score: " + str(high_score), "Game Starting!"]
-    for text in start_messages:
-        print(text)
+    # List & For Loop use for showing massage at starting time 
+    
+    status_updates = ["Checking Files...", "High Score: " + str(high_score), "Game Loading!"]
+    for status in status_updates:
+        print(status)
 
     game = GameEngine(screen)
 
-    # While Loop for game startswith
-    
+    # playing game 
     running = True
     while running:
+        
+        
+        # Filling screen with BLACK color 
+        
+        screen.fill(BLACK) 
+        
         score = game.run()
         data_handler.save_score(score)
         running = False 
-    #when game closed loop is over
 
     pygame.quit()
 
